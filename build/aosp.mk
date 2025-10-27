9ade8a5c5f81965e3c37591f3b6d2aa249c2de20
Ma Lingshu <61415309+ma0shu@users.noreply.github.com>
2025-10-27 19:41:17 +0800

    Add IMS product inheritance to aosp.mk

--- build/aosp.mk
+++ build/aosp.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, vendor/ponces/config/common.mk)
+$(call inherit-product, vendor/mediatek/ims/ims.mk)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_aosp/android-16.0/config/ota.json
